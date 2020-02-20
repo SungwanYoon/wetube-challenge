7e8fd7404c655c95812c2192230d13f095017121
SungwanYoon <57616919+SungwanYoon@users.noreply.github.com>
2020-02-21 01:34:25 +0900

    20.02.20 v2

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-const express = require("express");
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+
 const app = express();
 
 const PORT = 4000;
@@ -6,12 +11,28 @@ const PORT = 4000;
 const handleListening = () =>
   console.log(`Listening on: http://localhost:${PORT}`);
 
-const handleProfile = (req, res) => res.send("Hello Profile");
+const handleHome = (req, res) => res.send("Home.");
+const handleAboutUs = (req, res) => res.send("About Us.");
+const handleContact = (req, res) => res.send("Contact.");
+const handleProtected = (req, res) => res.send("Protected.");
 
-const handleHome = (req, res) => res.send("Hello from home");
+const betweenHome = (req, res, next) => {
+  console.log("I'm a middleware");
+  next();
+};
 
-app.get("/profile", handleProfile);
+const notAllow = (req, res, next) => res.redirect("/");
 
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan("combined"));
+
+app.use(betweenHome);
 app.get("/", handleHome);
+app.get("/about-us", handleAboutUs);
+app.get("/contact", handleContact);
+app.get("/protected", notAllow, handleProtected);
 
 app.listen(PORT, handleListening);
